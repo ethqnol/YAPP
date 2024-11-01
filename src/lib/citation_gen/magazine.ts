e1f25b0c434e86525889cdc0286b33e92a7fac67
Threeleaf <laserfun2907@gmail.com>
2024-11-01 16:02:37 -0400

    Citation Gen Mag./News.
    Added citation generation for Magazine / Newspaper source tyoes

--- src/lib/citation_gen/magazine.ts
+++ src/lib/citation_gen/magazine.ts
@@ -1,5 +1,25 @@
 import type Source from "../source";
+import type { Magazine } from "../specific_sources/magazine";
 
 export default function generate_magazine_citation(source: Source): string {
-  return "unimplemented"
+  let full_citation = "";
+  let formatted_authors : string[] = [];
+  (source.source_specific as Magazine).authors.forEach((author, index) => {
+    let author_format = ""
+    if (index == 0) {
+      let author_name = author.trim().split(" ")[0];
+      author_format = author.trim().split(" ").slice(1).join(" ") ? author.trim().split(" ").slice(1).join(" ") + ", " + author_name : author_name;
+    } else {
+      author_format = author.trim();
+    }
+
+    formatted_authors.push(author_format);
+});
+  full_citation += formatted_authors.join(", ") + ". ";
+  full_citation+= (source.source_specific as Magazine).title != "" ? "\"" + (source.source_specific as Magazine).title + ".\"" : "";
+  full_citation += (source.source_specific as Magazine).publication != "" ? "*" + (source.source_specific as Magazine).publication + "*" : "";
+  full_citation += (source.source_specific as Magazine).date_year ?  ", " + (source.source_specific as Magazine).date_year + ". " : "n.d. ";
+  full_citation += (source.source_specific as Magazine).identifier ? (source.source_specific as Magazine).identifier + "." : "";
+
+  return full_citation;
 }
--- src/lib/citation_gen/newspaper.ts
+++ src/lib/citation_gen/newspaper.ts
@@ -1,5 +1,26 @@
 import type Source from "../source";
-
+import type { Newspaper } from "../specific_sources/newspaper";
 export default function generate_newspaper_citation(source: Source): string {
-  return "unimplemented"
+  let full_citation = "";
+  let formatted_authors : string[] = [];
+  (source.source_specific as Newspaper).authors.forEach((author, index) => {
+    let author_format = ""
+    if (index == 0) {
+      let author_name = author.trim().split(" ")[0];
+      author_format = author.trim().split(" ").slice(1).join(" ") ? author.trim().split(" ").slice(1).join(" ") + ", " + author_name : author_name;
+    } else {
+      author_format = author.trim();
+    }
+
+    formatted_authors.push(author_format);
+  });
+  full_citation += formatted_authors.join(", ") + ". ";
+  full_citation+= (source.source_specific as Newspaper).title != "" ? "\"" + (source.source_specific as Newspaper).title + ".\"" : "";
+  full_citation += (source.source_specific as Newspaper).publication != "" ? "*" + (source.source_specific as Newspaper).publication + "*" : "";
+  full_citation += (source.source_specific as Newspaper).date ?  ", " + (source.source_specific as Newspaper).date : "n.d. ";
+  full_citation += (source.source_specific as Newspaper).edition ? ", " + (source.source_specific as Newspaper).edition + "edition" : "";
+  full_citation += (source.source_specific as Newspaper).section ? ", sec. " + (source.source_specific as Newspaper).section + ". " : "";
+  full_citation += (source.source_specific as Newspaper).identifier ? (source.source_specific as Newspaper).identifier + "." : "";
+
+  return full_citation;
 }
