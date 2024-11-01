683dab1637a2cbf7e7a8cafc3c37f69752871205
ethqnol <112029223+ethqnol@users.noreply.github.com>
2024-11-01 16:03:45 -0400

    fixing citation logic

--- src/lib/citation_gen/magazine.ts
+++ src/lib/citation_gen/magazine.ts
@@ -3,8 +3,9 @@ import type { Magazine } from "../specific_sources/magazine";
 
 export default function generate_magazine_citation(source: Source): string {
   let full_citation = "";
+  let magazine = source.source_specific as Magazine;
   let formatted_authors : string[] = [];
-  (source.source_specific as Magazine).authors.forEach((author, index) => {
+  magazine.authors.forEach((author, index) => {
     let author_format = ""
     if (index == 0) {
       let author_name = author.trim().split(" ")[0];
@@ -16,10 +17,10 @@ export default function generate_magazine_citation(source: Source): string {
     formatted_authors.push(author_format);
 });
   full_citation += formatted_authors.join(", ") + ". ";
-  full_citation+= (source.source_specific as Magazine).title != "" ? "\"" + (source.source_specific as Magazine).title + ".\"" : "";
-  full_citation += (source.source_specific as Magazine).publication != "" ? "*" + (source.source_specific as Magazine).publication + "*" : "";
-  full_citation += (source.source_specific as Magazine).date_year ?  ", " + (source.source_specific as Magazine).date_year + ". " : "n.d. ";
-  full_citation += (source.source_specific as Magazine).identifier ? (source.source_specific as Magazine).identifier + "." : "";
+  full_citation+= magazine.title != "" ? "\"" + magazine.title + ".\"" : "";
+  full_citation += magazine.publication != "" ? "*" + magazine.publication + "*" : "";
+  full_citation += magazine.date_year ?  ", " + magazine.date_year + ". " : "n.d. ";
+  full_citation += magazine.identifier ? magazine.identifier + "." : "";
 
   return full_citation;
 }
--- src/lib/citation_gen/newspaper.ts
+++ src/lib/citation_gen/newspaper.ts
@@ -2,8 +2,9 @@ import type Source from "../source";
 import type { Newspaper } from "../specific_sources/newspaper";
 export default function generate_newspaper_citation(source: Source): string {
   let full_citation = "";
+  let newspaper = source.source_specific as Newspaper;
   let formatted_authors : string[] = [];
-  (source.source_specific as Newspaper).authors.forEach((author, index) => {
+  newspaper.authors.forEach((author, index) => {
     let author_format = ""
     if (index == 0) {
       let author_name = author.trim().split(" ")[0];
@@ -15,12 +16,12 @@ export default function generate_newspaper_citation(source: Source): string {
     formatted_authors.push(author_format);
   });
   full_citation += formatted_authors.join(", ") + ". ";
-  full_citation+= (source.source_specific as Newspaper).title != "" ? "\"" + (source.source_specific as Newspaper).title + ".\"" : "";
-  full_citation += (source.source_specific as Newspaper).publication != "" ? "*" + (source.source_specific as Newspaper).publication + "*" : "";
-  full_citation += (source.source_specific as Newspaper).date ?  ", " + (source.source_specific as Newspaper).date : "n.d. ";
-  full_citation += (source.source_specific as Newspaper).edition ? ", " + (source.source_specific as Newspaper).edition + "edition" : "";
-  full_citation += (source.source_specific as Newspaper).section ? ", sec. " + (source.source_specific as Newspaper).section + ". " : "";
-  full_citation += (source.source_specific as Newspaper).identifier ? (source.source_specific as Newspaper).identifier + "." : "";
+  full_citation+= newspaper.title != "" ? "\"" + newspaper.title + ".\"" : "";
+  full_citation += newspaper.publication != "" ? "*" + newspaper.publication + "*" : "";
+  full_citation += newspaper.date ?  ", " + newspaper.date : "n.d. ";
+  full_citation += newspaper.edition ? ", " + newspaper.edition + "edition" : "";
+  full_citation += newspaper.section ? ", sec. " + newspaper.section + ". " : "";
+  full_citation += newspaper.identifier ? newspaper.identifier + "." : "";
 
   return full_citation;
 }
